d857ed63202c0b7a2f57ad9bc3645a5b563b9d9c
al-kafi-sohag <5438942+euitsol@users.noreply.github.com>
2025-07-16 21:40:47 +0600

    Update how-to.blade.php

--- resources/views/frontend/home/sections/how-to.blade.php
+++ resources/views/frontend/home/sections/how-to.blade.php
@@ -1,31 +1,31 @@
 <section class="py-12 md:py-12 lg:py-20 bg-white">
     <div class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
-        <h2 class="text-2xl md:text-4xl font-bold gradient-text text-center mb-8">Trusted by Thousands of Families</h2>
+        <h2 class="text-4xl md:text-4xl font-bold gradient-text text-center mb-8">Trusted by Thousands of Families</h2>
         <div class="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-8">
             <div class="text-center">
                 <div class="bg-orange-100 md:p-5 p-3 rounded-full inline-block mb-4">
-                    <i data-lucide="clock" class="w-12 h-12 md:w-16 md:h-16 flex items-center justify-center mx-auto text-orange-500"></i>
+                    <i data-lucide="clock" class="w-14 h-14 md:w-16 md:h-16 flex items-center justify-center mx-auto text-orange-500"></i>
                 </div>
                 <h3 class="text-lg md:text-xl font-semibold mb-2">Only 2 Minutes</h3>
                 <p class="text-gray-600 text-sm md:text-base text-center">to See the Magic</p>
             </div>
             <div class="text-center">
                 <div class="bg-green-100 md:p-5 p-3 rounded-full inline-block mb-4">
-                    <i data-lucide="shield-user" class="w-12 h-12 md:w-16 md:h-16 flex items-center justify-center mx-auto text-green-500"></i>
+                    <i data-lucide="shield-user" class="w-14 h-14 md:w-16 md:h-16 flex items-center justify-center mx-auto text-green-500"></i>
                 </div>
                 <h3 class="text-lg md:text-xl font-semibold mb-2">Privacy First</h3>
                 <p class="text-gray-600 text-sm md:text-base text-center">Designed with Care</p>
             </div>
             <div class="text-center">
                 <div class="bg-blue-100 md:p-5 p-3 rounded-full inline-block mb-4">
-                    <i data-lucide="star" class="w-12 h-12 md:w-16 md:h-16 flex items-center justify-center mx-auto text-blue-500"></i>
+                    <i data-lucide="star" class="w-14 h-14 md:w-16 md:h-16 flex items-center justify-center mx-auto text-blue-500"></i>
                 </div>
                 <h3 class="text-lg md:text-xl font-semibold mb-2">750+ Reviews</h3>
                 <p class="text-gray-600 text-sm md:text-base text-center">5 Star Rating</p>
             </div>
             <div class="text-center">
                 <div class="bg-purple-100 md:p-5 p-3 rounded-full inline-block mb-4">
-                    <i data-lucide="heart" class="w-12 h-12 md:w-16 md:h-16 flex items-center justify-center mx-auto text-purple-500"></i>
+                    <i data-lucide="heart" class="w-14 h-14 md:w-16 md:h-16 flex items-center justify-center mx-auto text-purple-500"></i>
                 </div>
                 <h3 class="text-lg md:text-xl font-semibold mb-2">Keepsake</h3>
                 <p class="text-gray-600 text-sm md:text-base text-center">Forever</p>
