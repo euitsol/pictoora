e7b1737d71752778a9c08d0f0c7be33d47a3494e
al-kafi-sohag <85807115+al-kafi-sohag@users.noreply.github.com>
2025-08-30 18:56:00 +0600

    Redesign preview page and add placeholder SVG
    Introduces a new placeholder.svg image for book pages, refactors the preview page Blade template for improved layout and styling, and updates the JavaScript to support toggling between single and spread view modes. Also adds new Lucide icons to the bootstrap.js import list and cleans up unused code.

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { createIcons, Calendar,Home, User, Settings, Menu, X, Clock, ShieldUser, Star, Heart, Users, Shield, Truck, MapPin, ShieldCheck, Eye, Award, ChevronUp, ChevronDown, Gift, CheckCircle, Mail, Funnel, FunnelX, BookOpen, Languages, Share2, ArrowRight, ArrowLeft, Check, Printer, BadgeCheck, Bookmark, RotateCcw } from 'lucide';
+import { createIcons, Calendar,Home, User, Settings, Menu, X, Clock, ShieldUser, Star, Heart, Users, Shield, Truck, MapPin, ShieldCheck, Eye, Award, ChevronUp, ChevronDown, Gift, CheckCircle, Mail, Funnel, FunnelX, BookOpen, Languages, Share2, ArrowRight, ArrowLeft, Check, Printer, BadgeCheck, Bookmark, RotateCcw, BookOpenCheck, RectangleHorizontal, Layers } from 'lucide';
 import $ from 'jquery';
 import Swiper from 'swiper/bundle';
 import 'swiper/css/bundle';
@@ -48,9 +48,8 @@ createIcons({
         BadgeCheck,
         Bookmark,
         RotateCcw,
+        RectangleHorizontal,
+        BookOpenCheck,
+        Layers
     } // List all imported icons
-});
-
-$(function () {
-    console.log("jQuery is working!");
 });
