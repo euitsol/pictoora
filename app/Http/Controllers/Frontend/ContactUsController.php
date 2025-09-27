fa4eff81eb2bb225772e2ecbbf4037a343d2e9ed
Mahibullah Siddique <47253920+wh1t3-shadow@users.noreply.github.com>
2025-09-27 20:23:51 +0600

    review problem and fix without new page add
    i will add new pages to night

--- app/Http/Controllers/Frontend/ContactUsController.php
+++ app/Http/Controllers/Frontend/ContactUsController.php
@@ -8,6 +8,11 @@ use App\Http\Controllers\FontendBaseController;
 class ContactUsController extends FontendBaseController
 {
     public function index(){
+         $this->setupSEO(
+            'The ultimate policeman | A Story for Everyone, Tailored Just Right | Pictoora',
+            'Create magical stories where your child becomes the hero of their own adventure | Pictoora',
+            'pictoora, personalized storybooks, children books, books page, books landing page, products page',
+        );
         return view('frontend.contact_us.contact');
     }
 }
